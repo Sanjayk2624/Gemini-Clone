d23e2f18daf4581e10b27115ec4af440d2d5f0d9
Sanjay K <162978176+Sanjayk2624@users.noreply.github.com>
2025-02-28 18:35:33 +0530

    Gemini-Clone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  SideBar  from './Components/sideBar/sideBar'
+import Main from './Components/Main/Main'
+
+
+ const App = () => {
+  return (
+    <>
+      <SideBar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
